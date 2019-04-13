696323d5b3483f5a156569b670b140d9ead8ae25
zhang jianli <jlzhang@ybjslc05.ihep.ac.cn>
2019-04-13 16:55:53 +0800

    add background method

---
+++ lhaapy/irf/effective_erea.py
@@ -0,0 +1,14 @@
+import numpy as np
+import astropy.units as u
+from astropy.io import fits
+from astropy.table import Table
+
+
+class EffectiveAreaTable:
+    """Effective area table.
+
+    def __init__(self, energy_lo, energy_hi, data, meta=None):
+	pass
+
+    def energy(self):
+	pass
